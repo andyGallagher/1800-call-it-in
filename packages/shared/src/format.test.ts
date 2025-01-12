af3e727699882e09a6e46eb80ec27cdeacd45b2f
Andrew Gallagher <andrewtgallagherJS@gmail.com>
2025-01-12 12:05:00 -0800

    set up formatters, tests

---
+++ packages/shared/src/format.test.ts
@@ -0,0 +1,48 @@
+import { collapsed, removeNewlines, unindented } from "@/format";
+import { expect, test } from "vitest";
+
+test("removeNewLines", () => {
+    expect(removeNewlines`
+Test ${777}
+`).toEqual("Test 777");
+
+    expect(removeNewlines`
+
+Interesting
+
+`).toEqual(`Interesting`);
+
+    expect(removeNewlines`Test
+  `).toEqual(`Test`);
+    expect(removeNewlines`
+  Test`).toEqual(`Test`);
+    expect(removeNewlines`Test`).toEqual(`Test`);
+});
+
+test("unindented", () => {
+    expect(unindented`
+    Test ${777}
+  `).toEqual("Test 777");
+
+    expect(unindented`
+
+    Interesting
+
+  `).toEqual(`
+Interesting
+`);
+
+    expect(unindented`Test
+  `).toEqual(`Test`);
+    expect(unindented`
+  Test`).toEqual(`Test`);
+    expect(unindented`Test`).toEqual(`Test`);
+});
+
+test("collapsed", () => {
+    expect(collapsed`
+
+    Test ${777}
+
+  `).toEqual("Test 777");
+});
---
+++ packages/shared/src/format.ts
@@ -0,0 +1,95 @@
+/**
+ * Remove newlines in a tagged template literal.
+ *
+ * Used like: removeNewlines`
+ * my string
+ * won't have
+ * newlines
+ * `
+ *
+ * returns "my string won't have newlines"
+ */
+export const removeNewlines = (
+    rawStrings: TemplateStringsArray,
+    ...args: any[] | never
+) => {
+    // Interpolate template strings with variables
+    const joinedString = rawStrings
+        .flatMap((rawString, index) =>
+            index < rawStrings.length ? [rawString, args[index]] : rawString,
+        )
+        .join("");
+
+    // Split against newlines, filter out empty sequences, join with spaces, and trim.
+    return joinedString
+        .split("\n")
+        .filter((line) => line !== "")
+        .join(" ")
+        .trim();
+};
+
+/**
+ * Remove indents in a tagged template literal.
+ *
+ * Used like: unindented`
+ *     my string
+ *     won't have weird leading spaces
+ *     but will still have newlines
+ * `
+ *
+ * returns: "my string\nwon't have weird leading spaces\nbut will still have newlines"
+ */
+export const unindented = (
+    rawStrings: TemplateStringsArray,
+    ...args: any[] | never
+) => {
+    // Interpolate template strings with variables
+    const joinedString = rawStrings
+        .flatMap((rawString, index) =>
+            index < rawStrings.length ? [rawString, args[index]] : rawString,
+        )
+        .join("");
+
+    // Split against newlines
+    const splitLines = joinedString.split("\n");
+
+    if (splitLines[0].trim() === "") {
+        splitLines.shift();
+    }
+    if (splitLines[splitLines.length - 1].trim() === "") {
+        splitLines.pop();
+    }
+
+    // Remove leading and trailing spaces
+    const removedLeadingSpaces = splitLines
+        .map((line) => {
+            return line;
+        })
+        .filter((line): line is string => line !== undefined);
+
+    // Replace all tabs
+    const removedTabs = removedLeadingSpaces.map((line) =>
+        line.replace(/^\s+/g, ""),
+    );
+
+    // Join with newlines
+    return removedTabs.join("\n");
+};
+
+/**
+ * Remove indents and newlines in a tagged template literal.
+ *
+ * Used like: collapsed`
+ *     my string
+ *     won't have weird leading spaces
+ *     and also won't have newlines
+ * `
+ *
+ * returns: "my string won't have weird leading spaces and also won't have newlines"
+ */
+export const collapsed = (
+    rawStrings: TemplateStringsArray,
+    ...args: any[] | never
+) => {
+    return removeNewlines`${unindented(rawStrings, ...args)}`;
+};
