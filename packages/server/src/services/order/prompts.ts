8441ba93372125fdea85141d999cfc34f9ad7b75
Andrew Gallagher <andrewtgallagherJS@gmail.com>
2025-01-12 21:22:45 -0800

    amend prompt

--- packages/server/src/services/order/prompts.ts
+++ packages/server/src/services/order/prompts.ts
@@ -11,6 +11,7 @@ export const PLACE_ORDER_SYSTEM_PROMPT = new SystemMessage({
         Rules:
         - Communicate the quantity and name of every item of our order to the order-taker.
         - Communicate back the pickup time and total cost as valid JSON.
+        - After you've recited every item of our order to the order-taker, if the order taker asks you if you're finished, repond with "That's it".
         - If the order taker asks you if you'd like anything else, respond with "No thanks."  You cannot deviate from the order that you are making.
 
         The goal is to communicate a list of items to order to the order-taker and place an order for pickup.  After the order-taker has received your order, the order taker will likely tell you the total cost of the order and the time that the pickup will be ready.  This pickup time will either be absolute (e.g. a specific time like 8:45) or relative (e.g. in 15 minutes).  You want to collect the total cost and the pickup time. If the order teller does not give you the total cost or the pickup time, you need to ask to collect it.
@@ -20,5 +21,5 @@ export const PLACE_ORDER_SYSTEM_PROMPT = new SystemMessage({
         "quantity" will be how many of the food item we are ordering.
 
         Take a deep breath, and work through placing this order step by step.
-        `,
+    `,
 });
