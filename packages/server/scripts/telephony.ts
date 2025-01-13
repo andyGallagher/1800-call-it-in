f05046ff7e5b6ef2cbc5d7bbeb3070a23570c492
Andrew Gallagher <andrewtgallagherJS@gmail.com>
2025-01-12 17:20:09 -0800

    mock out a phonecall

--- packages/server/scripts/telephony.ts
+++ packages/server/scripts/telephony.ts
@@ -4,20 +4,52 @@
  * and we have a UI for this.
  */
 
+import { order } from "@/services/order";
 import { telephony } from "@/services/telephony";
-import { notImplemented } from "shared/src/function";
+import { CompleteOrder } from "schema";
 
+/**
+ * Test out a phonecall
+ */
 void (async () => {
     const args = process.argv.slice(2);
     const command = args[0];
 
     switch (command) {
-        case "phoneNumber":
-            const phoneNumberId = await telephony.getPhoneNumberId("Vapi");
-            console.info({ phoneNumberId });
-            return;
         case "call":
-            return notImplemented();
+            const stubOrder: CompleteOrder = {
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                id: "xxx",
+                restaurantName: "McDonalds",
+                restaurantPhoneNumber: "+1234567890",
+                pickupTime: null,
+                totalCost: null,
+                menuItems: [
+                    {
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                        id: "fff",
+                        name: "Big Mac",
+                        quantity: 1,
+                        orderId: "xxx",
+                        costPerItem: 5.99,
+                    },
+                ],
+            };
+
+            const firstMessage = order.create.prompt.firstMessage(stubOrder);
+            const template = order.create.prompt.template(stubOrder);
+
+            const call = await telephony.call(
+                "Vapi",
+                "+1234567890",
+                firstMessage,
+                template,
+            );
+            console.info({ call });
+            return;
+
         default: {
             throw new Error("Invalid command");
         }
