b20a5ff5f58fdcfafe5a9b42631cca88d976c5d3
Andrew Gallagher <andrewtgallagherJS@gmail.com>
2025-01-14 13:12:50 -0800

    add fetch wrapper

---
+++ packages/extension/src/util/fetch.ts
@@ -0,0 +1,56 @@
+import { ZodSchema } from "zod";
+
+const BASE_API_URL: string = import.meta.env.VITE_API_URL;
+
+const joinUrls = (baseUrl: string, relativeUrl: string): string => {
+    if (!baseUrl.endsWith("/")) {
+        baseUrl += "/";
+    }
+    if (relativeUrl.startsWith("/")) {
+        relativeUrl = relativeUrl.substring(1);
+    }
+    return baseUrl + relativeUrl;
+};
+
+const fetchWrapper = async <T, B>(
+    relativeUrl: string,
+    method: string,
+    schemas: {
+        input?: ZodSchema<B>;
+        output: ZodSchema<T>;
+    },
+    body?: B,
+): Promise<T> => {
+    if (schemas.input) {
+        if (!body) {
+            throw new Error("fetch: body is required");
+        }
+
+        const input = schemas.input.safeParse(body);
+        if (!input.success) {
+            throw new Error("fetch: input validation failed");
+        }
+    }
+
+    const options: RequestInit = {
+        method,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: body ? JSON.stringify(body) : undefined,
+    };
+
+    const url = joinUrls(BASE_API_URL, relativeUrl);
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+        throw new Error(`fetch: HTTP error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const parsedData = schemas.output.parse(data);
+
+    return parsedData;
+};
+
+export { fetchWrapper as fetch };
