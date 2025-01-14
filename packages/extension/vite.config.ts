0c454123cc521825747bbd23ff6200664b02745b
Andrew Gallagher <andrewtgallagherJS@gmail.com>
2025-01-13 20:30:14 -0800

    favor dist

--- packages/extension/vite.config.ts
+++ packages/extension/vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
         },
     },
     build: {
-        outDir: "build",
+        outDir: "dist",
         rollupOptions: {
             input: {
                 main: "./index.html",
