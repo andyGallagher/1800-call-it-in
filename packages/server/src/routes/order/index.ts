da9ca322810db3db3f4c9abd8d2d70d068192ebd
Andrew Gallagher <andrewtgallagherJS@gmail.com>
2025-01-14 17:06:20 -0800

    fix cache logic

--- packages/server/src/routes/order/index.ts
+++ packages/server/src/routes/order/index.ts
@@ -4,7 +4,12 @@ import { order } from "@/services/order";
 import { DEFAULT_TELEPHONY_PROVIDER, telephony } from "@/services/telephony";
 import { createHash } from "crypto";
 import { Hono } from "hono";
-import { CreateOrderInput, ParseOrderInput, PhoneNumber } from "schema";
+import {
+    CreateOrderInput,
+    ParsedMenuItem,
+    ParseOrderInput,
+    PhoneNumber,
+} from "schema";
 import { unindented } from "shared/src/format";
 import { notImplemented } from "shared/src/function";
 
@@ -18,73 +23,72 @@ orderRouter.get("/:id", (c) => {
     return notImplemented();
 });
 
-orderRouter
-    .post("/", makeValidator(CreateOrderInput), async (c) => {
-        const rawOrder = c.req.valid("json");
+orderRouter.post("/", makeValidator(CreateOrderInput), async (c) => {
+    const rawOrder = c.req.valid("json");
 
-        if (!rawOrder.parsedMenuItems.length) {
-            return c.json({ error: [{ message: "No menu items" }] }, 400);
-        }
+    if (!rawOrder.parsedMenuItems.length) {
+        return c.json({ error: [{ message: "No menu items" }] }, 400);
+    }
 
-        const isOutBoundPhoneNumberValid = PhoneNumber.safeParse(
-            rawOrder.restaurantPhoneNumber,
-        ).success;
+    const isOutBoundPhoneNumberValid = PhoneNumber.safeParse(
+        rawOrder.restaurantPhoneNumber,
+    ).success;
 
-        if (!isOutBoundPhoneNumberValid) {
-            throw new Error(
-                unindented`
+    if (!isOutBoundPhoneNumberValid) {
+        throw new Error(
+            unindented`
                     vapi.phone.call: Invalid phone number: ${rawOrder.restaurantPhoneNumber}.
                     Hot tip: you probably are missing US country code or leading plus sign (+1).
                 `,
-            );
-        }
-
-        const dbOrder = await db.order.create({
-            data: {
-                ...rawOrder,
-
-                menuItems: {
-                    create: rawOrder.parsedMenuItems,
-                },
-            },
-            include: {
-                menuItems: true,
-            },
-        });
-
-        const firstMessage = order.create.prompt.firstMessage(dbOrder);
-        const template = order.create.prompt.template(dbOrder);
-
-        const call = await telephony.call(
-            DEFAULT_TELEPHONY_PROVIDER,
-            dbOrder.restaurantPhoneNumber,
-            firstMessage,
-            template,
         );
+    }
 
-        const updatedOrder = await db.order.update({
-            data: {
-                telephoneCall: {
-                    create: {
-                        externalServiceId: call.telephoneCallExternalServiceId,
-                        externalServiceType:
-                            call.telephoneCallExternalServiceType,
-                    },
+    const dbOrder = await db.order.create({
+        data: {
+            userName: rawOrder.userName,
+            userPhoneNumber: rawOrder.userPhoneNumber,
+            restaurantName: rawOrder.restaurantName,
+            restaurantPhoneNumber: rawOrder.restaurantPhoneNumber,
+
+            menuItems: {
+                create: rawOrder.parsedMenuItems.map((parsedMenuItem) => ({
+                    name: parsedMenuItem.name,
+                    quantity: parsedMenuItem.quantity,
+                    costPerItem: parsedMenuItem.costPerItem,
+                })),
+            },
+        },
+        include: {
+            menuItems: true,
+        },
+    });
+
+    const firstMessage = order.create.prompt.firstMessage(dbOrder);
+    const template = order.create.prompt.template(dbOrder);
+
+    const call = await telephony.call(
+        DEFAULT_TELEPHONY_PROVIDER,
+        dbOrder.restaurantPhoneNumber,
+        firstMessage,
+        template,
+    );
+
+    const updatedOrder = await db.order.update({
+        data: {
+            telephoneCall: {
+                create: {
+                    externalServiceId: call.telephoneCallExternalServiceId,
+                    externalServiceType: call.telephoneCallExternalServiceType,
                 },
             },
-            where: {
-                id: dbOrder.id,
-            },
-        });
-
-        return c.json(updatedOrder);
-    })
-    .put((c) => {
-        return notImplemented();
-    })
-    .delete((c) => {
-        return notImplemented();
+        },
+        where: {
+            id: dbOrder.id,
+        },
     });
+
+    return c.json(updatedOrder);
+});
 
 orderRouter.post("/parse", makeValidator(ParseOrderInput), async (c) => {
     const { rawContent, refresh } = c.req.valid("json");
@@ -93,11 +97,20 @@ orderRouter.post("/parse", makeValidator(ParseOrderInput), async (c) => {
         where: {
             inputHash: hash(rawContent),
         },
+        orderBy: {
+            createdAt: "desc",
+        },
     });
 
     // This is not very typesafe, but it's fine for now.
-    if (refresh !== true && cached) {
-        return c.json(cached.outputData);
+    if (cached && Array.isArray(cached.outputData)) {
+        const parsedMenuItems = ParsedMenuItem.array().safeParse(
+            cached.outputData,
+        );
+
+        if (parsedMenuItems.success && !refresh) {
+            return c.json(cached.outputData);
+        }
     }
 
     const menuItems = await order.parse.menuItems(rawContent);
