8ca233f1e8f87ec717c88d889cb6ab3b0f9fd7e7
Andrew Gallagher <andrewtgallagherJS@gmail.com>
2025-01-15 18:05:25 -0800

    actually we want telephone calls on this

--- packages/schema/src/complete.ts
+++ packages/schema/src/complete.ts
@@ -1,8 +1,13 @@
 /* eslint-disable no-relative-import-paths/no-relative-import-paths */
 import { z } from "zod";
-import { MenuItemModel, OrderModel } from "./generated-zod-schemas";
+import {
+    MenuItemModel,
+    OrderModel,
+    TelephoneCallModel,
+} from "./generated-zod-schemas";
 
 export interface CompleteOrder extends z.infer<typeof CompleteOrder> {}
 export const CompleteOrder = OrderModel.extend({
     menuItems: MenuItemModel.array(),
+    telephoneCall: TelephoneCallModel.nullable(),
 });
